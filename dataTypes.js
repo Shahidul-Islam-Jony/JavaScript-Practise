07ceea67812b744ae69986682abd0b1c9a10800c
Shahidul-Islam-Jony <shahidulislam13sf@gmail.com>
2024-04-26 20:13:51 +0600

    Javascript data types

---
+++ dataTypes.js
@@ -0,0 +1,20 @@
+// JavaScript has Two type of Data
+/**
+ * 1.Premitive
+ * 2.Non-Premitive
+ */
+
+// Premitive data type has 5 type of data ,They are
+/**
+ * 1.Numbers
+ * 2.Strings
+ * 3.Boolean
+ * 4.Null
+ * 5.Undefinied
+ */
+
+// Non-Premitive has 2 types of data
+/**
+ * 1.Objects
+ * 2.Array
+ */
